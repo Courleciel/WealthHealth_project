05f1b97ba867e280ae26b3b885cc41d349157a77
luc perrot <luc.perrot@hotmail.fr>
2024-11-22 09:41:39 +0100

    emploee still exist after page refresh

--- wealthhealth_project/src/store.js
+++ wealthhealth_project/src/store.js
@@ -1,11 +1,17 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
+const loadFromLocalStorage = () => {
+  const data = localStorage.getItem("employees");
+  return data ? JSON.parse(data) : [];
+};
+
 const employeesSlice = createSlice({
   name: "employees",
-  initialState: [],
+  initialState: loadFromLocalStorage(),
   reducers: {
     addEmployee: (state, action) => {
       state.push(action.payload);
+      localStorage.setItem("employees", JSON.stringify(state));
     },
   },
 });
